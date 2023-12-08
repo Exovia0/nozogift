fd00987a43c7f9dfc47fd27e6d634df5d5d5c419
Exovia0 <103326461+Exovia0@users.noreply.github.com>
2023-12-08 17:33:32 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function showPreview() {
+    document.querySelector('.preview-overlay').style.display = 'flex';
+}
+
+function hidePreview() {
+    document.querySelector('.preview-overlay').style.display = 'none';
+}
